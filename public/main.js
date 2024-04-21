9d703af906b02d2563bfa2518915f81790c7a2e8
Patrick Axinte <axinte.re.patrick@student.utcluj.ro>
2024-04-21 16:44:22 +0300

    modified index.html

--- public/main.js
+++ public/main.js
@@ -7,6 +7,31 @@ document.getElementById('togglePortfolio').addEventListener('click', function() 
         portfolioSection.style.display = 'none';
     }
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomGradient() {
+    var color1 = getRandomColor();
+    var color2 = getRandomColor();
+    document.body.style.background = 'linear-gradient(to right, ' + color1 + ', ' + color2 + ') !important';
+
+}
+
+// Set a random gradient background on load and each refresh
+window.onload = setRandomGradient;
+
+document.addEventListener('DOMContentLoaded', function() {
+    setRandomGradient();
+    console.log('Random gradient set');
+});
+
 // JavaScript to handle form submission and display message
 document.querySelector('.contact-form form').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -29,3 +54,7 @@ document.querySelector('.contact-form form').addEventListener('submit', function
         console.error('Error:', error);
     });
 });
+
+
+
+
