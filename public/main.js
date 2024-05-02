fb388a26a6f904db5eb81ed99989c6c25c6db147
Patrick Axinte <axinte.re.patrick@student.utcluj.ro>
2024-05-02 15:03:50 +0300

    modified main.js

--- public/main.js
+++ public/main.js
@@ -7,29 +7,3 @@ document.getElementById('togglePortfolio').addEventListener('click', function() 
         portfolioSection.style.display = 'none';
     }
 });
-
-
-// JavaScript to handle form submission and display message
-document.querySelector('.contact-form form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var formData = new FormData(this);
-    fetch('/send', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        var formMessage = document.getElementById('formMessage');
-        formMessage.innerText = data.message;
-        formMessage.classList.add('show-message');
-        // Remove the message after 4 seconds
-        setTimeout(function() {
-            formMessage.classList.remove('show-message');
-            // Reset the form
-            document.querySelector('.contact-form form').reset();
-        }, 4000);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
